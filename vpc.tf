01f79e056be993e7cedf977be6c4dc76a67d668a
Jake Watkins <jwatkins@mozilla.com>
2016-05-25 16:27:18 -0700

    Add routes for elc, rds subnets

--- vpc.tf
+++ vpc.tf
@@ -34,16 +34,6 @@ resource "aws_route" "mozreview_public_igw-rtr" {
 #TODO: tags
 }
 
-# Setup private subnets for webheads
-resource "aws_subnet" "web_subnet" {
-  vpc_id = "${aws_vpc.mozreview_vpc.id}"
-  cidr_block = "${element(split(",", var.web_subnets), count.index)}"
-  availability_zone = "${element(split(",", var.web_azs), count.index)}"
-  count = "${length(compact(split(",", var.web_subnets)))}"
-#  tags { Name = "${var.name}-private" }
-#TODO: tags
-}
-
 # Setup public subnets for elb
 resource "aws_subnet" "elb_subnet" {
   vpc_id = "${aws_vpc.mozreview_vpc.id}"
@@ -56,6 +46,29 @@ resource "aws_subnet" "elb_subnet" {
   map_public_ip_on_launch = true
 }
 
+resource "aws_route_table_association" "elb" {
+  count = "${length(compact(split(",", var.elb_subnets)))}"
+  subnet_id = "${element(aws_subnet.elb_subnet.*.id, count.index)}"
+  route_table_id = "${aws_route_table.mozreview_public-rt.id}"
+}
+
+
+# Setup private subnets for webheads
+resource "aws_subnet" "web_subnet" {
+  vpc_id = "${aws_vpc.mozreview_vpc.id}"
+  cidr_block = "${element(split(",", var.web_subnets), count.index)}"
+  availability_zone = "${element(split(",", var.web_azs), count.index)}"
+  count = "${length(compact(split(",", var.web_subnets)))}"
+#  tags { Name = "${var.name}-private" }
+#TODO: tags
+}
+
+resource "aws_route_table_association" "web" {
+  count = "${length(compact(split(",", var.web_subnets)))}"
+  subnet_id = "${element(aws_subnet.web_subnet.*.id, count.index)}"
+  route_table_id = "${aws_route_table.mozreview_private-rt.id}"
+}
+
 # Setup private subnets for rds
 resource "aws_subnet" "rds_subnet" {
   vpc_id = "${aws_vpc.mozreview_vpc.id}"
@@ -64,6 +77,12 @@ resource "aws_subnet" "rds_subnet" {
   count = "${length(compact(split(",", var.rds_subnets)))}"
 #  tags { Name = "${var.name}-public" }
 #TODO: tags
+}
+
+resource "aws_route_table_association" "rds" {
+  count = "${length(compact(split(",", var.rds_subnets)))}"
+  subnet_id = "${element(aws_subnet.rds_subnet.*.id, count.index)}"
+  route_table_id = "${aws_route_table.mozreview_private-rt.id}"
 }
 
 # Setup private subnets for elasticache
@@ -76,16 +95,9 @@ resource "aws_subnet" "elc_subnet" {
 #TODO: tags
 }
 
-
-resource "aws_route_table_association" "web" {
-  count = "${length(compact(split(",", var.web_subnets)))}"
-  subnet_id = "${element(aws_subnet.web_subnet.*.id, count.index)}"
+resource "aws_route_table_association" "elc" {
+  count = "${length(compact(split(",", var.elc_subnets)))}"
+  subnet_id = "${element(aws_subnet.elc_subnet.*.id, count.index)}"
   route_table_id = "${aws_route_table.mozreview_private-rt.id}"
-}
-
-resource "aws_route_table_association" "elb" {
-  count = "${length(compact(split(",", var.elb_subnets)))}"
-  subnet_id = "${element(aws_subnet.elb_subnet.*.id, count.index)}"
-  route_table_id = "${aws_route_table.mozreview_public-rt.id}"
 }
 
