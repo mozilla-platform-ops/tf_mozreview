5510a289436d330e97a7a1856539467684446444
Jake Watkins <jwatkins@mozilla.com>
2016-06-02 10:36:12 -0700

    updated variables w/desc

--- variables.tf
+++ variables.tf
@@ -1,31 +1,91 @@
-variable "name" {
-    default = "mozreivew"
+variable "env" {
+    description = "Name of the product/envrionment"
 }
 
-variable "env" {}
-variable "vpc_cidr" { }
+variable "vpc_cidr" {
+    description = "CIDR block to assign to mozreview vpc"
+}
 
-variable "web_instance_type" {}
-variable "web_subnets" {}
-variable "web_azs" {}
-variable "web_instance_name" {}
-variable "web_ami_id" {}
-variable "web_instances_per_subnet" {}
-variable "web_instance_profile" {}
-variable "elb_subnets" {}
-variable "elb_azs" {}
-variable "rds_subnets" {}
-variable "rds_azs" {}
-variable "rds_instance_class" {}
-variable "logging_bucket" {}
-variable "memcached_instance_type" {}
-variable "elc_subnets" {}
-variable "elc_azs" {}
-variable "num_memcached_nodes" {}
-variable "allow_bastion_sg" {}
+variable "web_instance_type" {
+    description = "EC2 instance type of Mozreview web ec2 instances"
+}
 
+variable "web_subnets" {
+    description = "List of subnets for Mozreview web ec2 instances"
+}
 
-variable "peer_vpc_id" {}
-variable "peer_route_table_id" {}
-variable "peer_cidr_block" {} 
-variable "peer_account_id" {}
+variable "web_azs" {
+    description = "List of availablity zones to be matched with the web subnet list"
+}
+
+variable "web_ami_id" {
+    description = "AMI ID for Mozreview web ec2 instances"
+}
+
+variable "web_instances_per_subnet" {
+    description = "Number of web ec2 instances per web subnet"
+}
+
+variable "web_instance_profile" {
+    description = "Instance profile for Mozreview web ec2 instances"
+}
+
+variable "elb_subnets" {
+    description = "List of subnets for loadbalancer distribution"
+}
+
+variable "elb_azs" {
+    description = "List of availablity zones to be matched with the elb subnet list"
+}
+
+variable "rds_subnets" {
+    description = "List of subnets for database instances distribution"
+}
+
+variable "rds_azs" {
+    description = "List of availablity zones to be matched to the rds subnet list"
+}
+
+variable "rds_instance_class" {
+    description = "RDS instances class type"
+}
+
+variable "logging_bucket" {
+    description = "S3 logging bucket"
+}
+
+variable "memcached_instance_type" {
+    description = "Instance type for memcached instances"
+}
+
+variable "elc_subnets" {
+    description = "List of subnets for memcached instances distribution"
+}
+
+variable "elc_azs" {
+    description = "List of availablity zones to be matched to the elc subnet list"
+}
+
+variable "num_memcached_nodes" {
+    description = "Total number of memcached nodes"
+}
+
+variable "allow_bastion_sg" {
+    description = "Security group to allow bastion host access"
+}
+
+variable "peer_vpc_id" {
+    description = "Bastion host vpc id"
+}
+
+variable "peer_route_table_id" {
+    description = "Bastion host route table"
+}
+
+variable "peer_cidr_block" {
+    description = "Bastion host vpc CIDR block"
+}
+
+variable "peer_account_id" {
+    description = "Bastion host account number"
+}
