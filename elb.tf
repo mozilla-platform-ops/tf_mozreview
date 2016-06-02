96100733b64d03a36b87fb6280d311b935848dbf
Jake Watkins <jwatkins@mozilla.com>
2016-06-02 09:54:59 -0700

    enable logging and health check on elb

--- elb.tf
+++ elb.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "mozreview_elb-sg" {
 # Setup ELB for mozreview webheads
 # Create a new load balancer
 resource "aws_elb" "mozreview_web_elb" {
-    name = "${var.env}-elb"
+    name = "${var.env}-mozreview-elb"
     subnets = ["${aws_subnet.elb_subnet.*.id}"]
     security_groups = ["${aws_security_group.mozreview_elb-sg.id}"]
 
@@ -41,21 +41,36 @@ resource "aws_elb" "mozreview_web_elb" {
         lb_port = 80
         lb_protocol = "http"
     }
-
+/*
+    listener {
+        instance_port = 443
+        instance_protocol = "https"
+        lb_port = 443
+        lb_protocol = "https"
+        ssl_certificate_id = ""
+    }
+*/
     instances = ["${aws_instance.web_ec2_instance.*.id}"]
     cross_zone_load_balancing = true
     idle_timeout = 400
     connection_draining = true
     connection_draining_timeout = 400
-/*
+
     # Logging
     access_logs {
         bucket = "${var.logging_bucket}"
-        bucket_prefix = "elb/web-${var.env}"
         interval = 60
     }
-*/
-# TODO: Health Checks
+
+    # Health Checks
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        target = "TCP:80"
+        interval = 30
+    }
+
     tags {
         Name = "${var.env}-mozreview-elb"
     }
